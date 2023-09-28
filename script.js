3cc8b748fc1662fb2d1aff7b57b7475d9fd34663
Mateusz Lis <lismateusz2005@gmail.com>
2023-09-28 16:32:56 +0200

    some of the special keys doesnt work yet but its almost ok

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-const keys = document.querySelectorAll(".key");
+document.addEventListener("DOMContentLoaded", () => {
+    const keys = document.querySelectorAll(".key");
 
-document.addEventListener("keydown", (event) => {
-    const keyName = event.key.toUpperCase();
-    keys.forEach((key) => {
-        const keyText = key.textContent;
+    document.addEventListener("keydown", (event) => {
+        const keyName = event.key.toUpperCase();
+        keys.forEach((key) => {
+            const keyText = key.textContent;
 
-        if (keyText === keyName) {
-            key.style.backgroundColor = "red";
-        }
+            if (
+                keyText === keyName ||
+                key.id.toLowerCase() === keyName.toLowerCase()
+            ) {
+                key.style.backgroundColor = "red";
+            }
+        });
     });
-});
 
-document.addEventListener("keyup", (event) => {
-    const keyName = event.key.toUpperCase();
+    document.addEventListener("keyup", (event) => {
+        const keyName = event.key.toUpperCase();
 
-    keys.forEach((key) => {
-        const keyText = key.textContent;
+        keys.forEach((key) => {
+            const keyText = key.textContent;
 
-        if (keyText === keyName) {
-            key.style.backgroundColor = "#fff";
-        }
+            if (
+                keyText === keyName ||
+                key.id.toLowerCase() === keyName.toLowerCase()
+            ) {
+                key.style.backgroundColor = "#fff";
+            }
+        });
     });
 });
